f35baae768cad07a7b1fa3f26cc29ebdc43f0111
JonasZed <jozim@bluewin.ch>
2024-04-09 13:57:35 +0200

    step 2 (untested)

--- done/imgfscmd.c
+++ done/imgfscmd.c
@@ -15,6 +15,13 @@
 #include <string.h>
 
 
+typedef int (*command)(int, char*[]);
+
+struct command_mapping {
+    const char* name;
+    command comm;
+};
+
 /*******************************************************************************
  * MAIN
  */
@@ -25,12 +32,28 @@ int main(int argc, char* argv[])
     if (argc < 2) {
         ret = ERR_NOT_ENOUGH_ARGUMENTS;
     } else {
-        /* **********************************************************************
-         * TODO WEEK 07: THIS PART SHALL BE EXTENDED.
-         * **********************************************************************
-         */
-
         argc--; argv++; // skips command call name
+
+        int comm_qte = 4;
+        command chosen_comm = (void*);
+        command_mapping[] commands = {{"list", do_list_cmd},
+                                      {"create", do_create_cmd},
+                                      {"help", help},
+                                      {"delete", do_delete_cmd}};
+
+        for(int i = 0; i < comm_qte; ++i) {
+            if (strcmp(argv[0], commands[i][0]) == 0) {
+                chosen_comm = commands[i][1];
+            }
+        }
+        argc--; argv++; // option used, now it can be discarded
+        if (chosen_comm == (void*)) {
+            ret = ERR_INVALID_COMMAND;
+            help(argc, argv);
+        } else { // FIXME do we need to add a NULL to argv
+            chosen_comm(argc, argv);
+        }
+
     }
 
     if (ret) {
