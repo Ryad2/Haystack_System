4d6fbc05a4afcfcd315c1ca1d7ffe60fd689cff4
Ryad2 <ryad.aouak@gmail.com>
2024-04-11 20:30:54 +0200

    start of STEP2(create, delete, help) etape1

---
+++ done/imgfs_create.c
@@ -0,0 +1,9 @@
+#include "imgfs.h"
+#include "util.h"
+
+int do_create(const char* imgfs_filename, struct imgfs_file* imgfs_file){
+    M_REQUIRE_NON_NULL(imgfs_filename);
+    M_REQUIRE_NON_NULL(imgfs_file);
+
+    FILE* imgfs
+}
