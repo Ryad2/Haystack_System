c5bbefcbab34623dafe5d4e0c5994e88f41aa941
JonasZed <jozim@bluewin.ch>
2024-05-15 15:18:27 +0200

    finished week11

---
+++ done/imgfs_server_service.c
@@ -0,0 +1,55 @@
+/*
+ * @file imgfs_server_services.c
+ * @brief ImgFS server part, bridge between HTTP server layer and ImgFS library
+ *
+ * @author Konstantinos Prasopoulos
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h> // uint16_t
+
+#include "error.h"
+#include "util.h" // atouint16
+#include "imgfs.h"
+#include "http_net.h"
+#include "imgfs_server_service.h"
+
+// Main in-memory structure for imgFS
+static struct imgfs_file fs_file;
+static uint16_t server_port;
+
+#define URI_ROOT "/imgfs"
+
+/********************************************************************//**
+ * Startup function. Create imgFS file and load in-memory structure.
+ * Pass the imgFS file name as argv[1] and optionnaly port number as argv[2]
+ ********************************************************************** */
+int server_startup (int argc, char **argv)
+{
+    if (argc < 2) return ERR_NOT_ENOUGH_ARGUMENTS;
+    int errcode = ERR_NONE;
+    if ((errcode = do_open(argv[1], "rb", &fs_file))) {
+        return errcode;
+    }
+    print_header(&fs_file.header);
+
+    server_port = (uint16_t) http_init(argc > 2 ? atouint16(argv[2]) : DEFAULT_LISTENING_PORT, NULL);
+
+    printf("ImgFS server started on http://localhost:%d\n", server_port);
+    fflush(stdout);
+
+    return ERR_NONE;
+}
+
+/********************************************************************//**
+ * Shutdown function. Free the structures and close the file.
+ ********************************************************************** */
+void server_shutdown (void)
+{
+    fprintf(stderr, "Shutting down...\n");
+    http_close();
+    do_close(&fs_file);
+}
+
